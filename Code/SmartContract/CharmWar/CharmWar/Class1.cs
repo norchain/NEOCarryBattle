2d99c95500bba4a0f223ed34c913d098ae2af597
Daniel Pan <dprat0821@gmail.com>
2018-06-28 15:06:26 -0400

    add project

---
+++ Code/SmartContract/CharmWar/CharmWar/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace CharmWar
+{
+	public class Charm : SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
