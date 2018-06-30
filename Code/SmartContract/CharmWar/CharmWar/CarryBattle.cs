ab5ec0bb04682f83df01beec6f2c4a4e76e7f1b4
Daniel Pan <dprat0821@gmail.com>
2018-06-30 19:48:43 -0400

    new scripts

---
+++ Code/SmartContract/CharmWar/CharmWar/CarryBattle.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Helper = Neo.SmartContract.Framework.Helper;
+
+using System.ComponentModel;
+using System.Numerics;
+
+
+namespace CarryBattle
+{
+	[Serializable]
+	public class Card
+	{
+		//public BigInteger id;
+		public byte type;   //0: Infantry, 1: Archer, 2: Cavalry
+		public byte[] levels;    // Range: 0 - 255
+		public byte[] warID;   //0: Dead 1: Normal
+		public byte[] playerID;
+	}
+
+	[Serializable]
+	public class War{
+		//public BigInteger id;
+		public byte[] endHeight;
+		public byte[] cardIDs;  
+	}
+    
+	public class Constant{
+		static int bytesCardID = 20;
+	}
+
+	public class CarryBattle : SmartContract
+    {
+		public static byte[] Main(string operation, byte[] paras)
+        {
+			if (operation == "war"){
+				
+			}
+			byte[] r = new byte[1];
+			return r;
+        }
+       
+
+		/* ================
+        * Basic Get Information
+        ===================*/
+
+		public static byte[] SetMapSample(byte[]value, params object[] path){
+
+			StorageMap map = Storage.CurrentContext.CreateMap();
+
+		}
+
+		public static byte[] GetCardRaw(byte[] cardID){
+			return Storage.Get(Storage.CurrentContext, cardID);
+		}
+
+		public static byte[] GetWarRaw(byte[] warID)
+        {
+            return Storage.Get(Storage.CurrentContext, warID);
+        }
+        
+		public static byte[] GetCardsByUserId(byte[] playerId){
+			
+			//byte[] cardsID = GetCardRaw()
+			byte[] r = new byte[1];
+            return r; 
+		}
+
+
+
+
+		public static byte[] OpenRoom(byte[] playerID, BigInteger[] cardIDs)
+        {
+            
+
+        }
+
+		public static byte[] JoinRoom(byte[] playerID, BigInteger roomID, BigInteger[] cardIDs)
+        {
+
+        }
+
+
+		private static Card _GetCard(byte[] cardID){
+			byte[] raw = GetCardRaw(cardID);
+			if(raw.Length == 0){
+				return null;
+			}
+			else{
+				object[] objs = (object[])Helper.Deserialize(raw);
+				return (Card)(object)objs;
+
+			}
+		}
+
+      
+		private static War _GetWar(byte[] warID){
+			byte[] raw = GetWarRaw(warID);
+            if (raw.Length == 0){
+                return null;
+            }
+            else{
+                object[] objs = (object[])Helper.Deserialize(raw);
+				return (War)(object)objs;
+            }
+        }
+        
+        
+
+
+    }
+}
---
+++ Code/SmartContract/CharmWar/CharmWar/TestContract.cs
@@ -0,0 +1,36 @@
+﻿using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework;
+using System.Numerics;
+
+namespace Neo.SmartContract
+{
+    public class MapExample : Framework.SmartContract
+    {
+        public static byte[] Main(string op)
+        {
+            // Simple Map
+            Map<string, int> m = new Map<string, int>();
+            m["hello"] = 10;
+            m["world"] = 15;
+            Runtime.Notify(m["hello"]); // will print '10'
+            Runtime.Notify(m["world"]); // will print '15'
+
+            // StorageMap (create prefix for Storage)
+            StorageMap user_sm = Storage.CurrentContext.CreateMap("user"); // 'user' prefix
+            user_sm.Put("xyz", 20);
+            user_sm.Put("wrd", 30);
+            StorageMap tx_sm = Storage.CurrentContext.CreateMap("tx"); // 'tx' prefix
+            tx_sm.Put("xyz", 40);
+            tx_sm.Put("wrd", 50);
+
+            Runtime.Notify(user_sm.Get("xyz").AsBigInteger()); // will print '20'
+            Runtime.Notify(user_sm.Get("wrd").AsBigInteger()); // will print '30'
+            Runtime.Notify(tx_sm.Get("xyz").AsBigInteger()); // will print '40'
+            Runtime.Notify(tx_sm.Get("wrd").AsBigInteger()); // will print '50'
+            Runtime.Notify(Storage.Get(Storage.CurrentContext, "user\x00xyz").AsBigInteger()); // will print '20'
+			BigInteger r = Storage.Get(Storage.CurrentContext, "tx\u0000xyz").AsBigInteger();
+            Runtime.Notify(r); // will print '40'
+			return r.ToByteArray();
+        }
+    }
+}
