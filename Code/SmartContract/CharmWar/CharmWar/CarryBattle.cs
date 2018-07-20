6bd1cc724747211dda57ff4df4f495d8abbf87e9
Daniel Pan <dprat0821@gmail.com>
2018-07-19 20:42:21 -0400

    remove temporary code

--- Code/SmartContract/CharmWar/CharmWar/CarryBattle.cs
+++ Code/SmartContract/CharmWar/CharmWar/CarryBattle.cs
@@ -153,43 +153,7 @@ namespace CarryBattle
 				}
 			}
 
-			//Serialize function is not supported by Unity. Use the customized one
-			public static byte[] Serialize(Card card)
-            {
-				//byte type
-				//byte[] lvls
-				byte[] prtID1 = card.prtID1.ToByteArray();
-				byte[] prtID2 = card.prtID2.ToByteArray();
-				byte[] creator = card.creator.ToByteArray();
-				byte[] owner = card.owner.ToByteArray();
-				byte[] score = card.score.ToByteArray();
-				byte[] wins = card.wins.ToByteArray();
-				byte[] wars = card.wars.ToByteArray();
 
-				int length = 1 + 
-					Const.SizeCard* Const.SizeCard + 
-				         prtID1.Length + 
-				         prtID2.Length + 
-				         creator.Length + 
-				         owner.Length +
-
-				byte[] c = new byte[];
-            }
-
-            public static Card Deserialize(byte[] data)
-            {
-
-            }
-
-            public static byte[] Serialize(CardAct cardact)
-            {
-
-            }
-
-            public static Card Deserialize(byte[] data)
-            {
-
-            }
 
 			#endregion
 		}
